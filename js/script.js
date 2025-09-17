56708ba7d4a6076e0d6df8ce8835dba5510bd3f3
MarteDev-art <guilhermediasmartinelli2007@gmail.com>
2025-09-17 14:32:47 -0300

    Mudançageral

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
-const toggleBtn = document.getElementById("menu-toggle");
-const sidebar = document.getElementById("sidebar");
+const toggle = document.getElementById("menu-toggle");
+const menu = document.getElementById("menu");
 
-toggleBtn.addEventListener("click", () => {
-  sidebar.classList.toggle("open");
+toggle.addEventListener("click", () => {
+  menu.style.display = menu.style.display === "flex" ? "none" : "flex";
+});
+const temaBtn = document.getElementById("tema-toggle");
+const temas = ["tema-padrao", "tema-escuro", "tema-magico"];
+let atual = localStorage.getItem("tema") || "tema-padrao";
+
+document.body.classList.add(atual);
+document.querySelector("header").classList.add(atual);
+
+temaBtn.addEventListener("click", () => {
+  const index = (temas.indexOf(atual) + 1) % temas.length;
+  const novoTema = temas[index];
+
+  document.body.classList.remove(atual);
+  document.querySelector("header").classList.remove(atual);
+
+  document.body.classList.add(novoTema);
+  document.querySelector("header").classList.add(novoTema);
+
+  localStorage.setItem("tema", novoTema);
+  atual = novoTema;
 });
